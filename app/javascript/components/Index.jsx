3545225fd58cc36235acc4453eadb3c3b00877d9
T31K <t31kmunwong@gmail.com>
2022-02-11 16:24:56 +0800

    First commit

---
+++ app/javascript/components/Index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Index (props) {
+  return (
+    <React.Fragment>
+      <h1>hello from react</h1>
+    </React.Fragment>
+  )
+}
+
---
+++ app/javascript/components/Show.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Show (props) {
+  return (
+    <React.Fragment>
+      <h1>Show from react</h1>
+    </React.Fragment>
+  )
+}
+
